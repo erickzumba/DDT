b1163f6715b6ec466c3d2300300f7833411e9ae5
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2022-11-30 13:35:28 +0000

    Updated request code

--- src/contracts/Taxi.sol
+++ src/contracts/Taxi.sol
@@ -5,13 +5,12 @@ pragma solidity ^0.8.0;
 contract Taxi {
     string public name;
     uint public rideCount = 0;
+    uint public requestCount = 0;
     mapping(uint => Request) public requests;
 
     constructor()  {
         name = "CCT Taxi App";
             }
-
-
 
 
  struct Request {
@@ -40,6 +39,17 @@ contract Taxi {
 
     );
 
+    function createRequest(string memory _fromLocation, string memory _toLocation) public {
+    // Require a valid location
+    require(bytes(_fromLocation).length > 0);
+    require(bytes(_toLocation).length > 0);     
+    // Increment request count
+    requestCount ++;
+    // Create the request
+    requests[requestCount] = Request(requestCount, _fromLocation, _toLocation, msg.sender);
+    // Trigger an event
+    emit reqCreated(requestCount, _fromLocation,_toLocation, msg.sender);
+}
    // function startRide(string memory _riderName, string _fromLocation, string _toLocation) public {
         // Require a valid name
        // require(bytes(_riderName).length > 0);
