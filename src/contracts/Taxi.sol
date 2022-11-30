ad376b1c8b17bb1664f2236395a6eca9d391568b
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2022-11-30 13:19:15 +0000

    added project files

---
+++ src/contracts/Taxi.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Taxi {
+    string public name;
+    uint public rideCount = 0;
+    mapping(uint => Request) public requests;
+
+    constructor()  {
+        name = "CCT Taxi App";
+            }
+
+
+
+
+ struct Request {
+        uint id;      
+        string fromLocation;
+        string toLocation;
+        address rider;       
+    }
+
+    event reqCreated (
+        uint id,
+        string fromLocation,
+        string toLocation,
+        address rider       
+
+    );
+
+    struct Ride {
+        Request req;
+        address driver;       
+    }
+
+    event rideCreated (
+        Request req,
+        address driver       
+
+    );
+
+   // function startRide(string memory _riderName, string _fromLocation, string _toLocation) public {
+        // Require a valid name
+       // require(bytes(_riderName).length > 0);
+        // Increment product count
+       // rideCount ++;
+        // Create the product
+      //  rides[rideCount] = Ride(rideCount, _riderName, _fromLocation, _toLocation,  msg.sender );
+        // Trigger an event
+//emit ProductCreated(productCount, _name, _price, msg.sender, false);
+   // }
+
+
+
+
+
+
+
+}
